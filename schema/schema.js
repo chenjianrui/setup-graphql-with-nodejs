187fcbb8b86d9b2b8de66374493b2d514fd93f4e
chenjianrui <sky75217@gmail.com>
2020-09-19 16:41:39 +0800

    add company schema，且讓 User 與 company 產生關聯

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,15 @@
 const graphql = require('graphql')
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql
-const _ = require('lodash')
 const axios = require('axios')
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+})
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -9,7 +17,14 @@ const UserType = new GraphQLObjectType({
     // 定義型態
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      async resolve(parseValue, args){
+        const { data } = await axios(`http://localhost:3000/companies/${parseValue.companyId}`)
+        return data
+      }
+    }
   }
 })
 
